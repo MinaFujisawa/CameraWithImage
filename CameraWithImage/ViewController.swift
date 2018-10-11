41e1bdfbb1b2d71d0d2729c1911530d6b5b25e27
Mina Fujisawa <mina@formathletica.com>
2018-10-10 19:11:22 -0700

    First init

---
+++ CameraWithImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraWithImage
+//
+//  Created by MINA FUJISAWA on 2018/10/10.
+//  Copyright © 2018 MINA FUJISAWA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
